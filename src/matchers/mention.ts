fe7195b3b9f64e4de898e2b9a2d6cb529607e895
Scott Gibbs <scott@gibbz.biz>
2021-08-23 17:00:02 +0200

    Added dedicated 'mention' argument types

--- src/matchers/mention.ts
+++ src/matchers/mention.ts
@@ -1,12 +1,15 @@
+import { NameArg } from "../types"
 import { ArgParseResponse } from "./base"
 import { RegexArg } from "./regex"
+
+export type MentionArgs = NameArg
 
 /**
  * Matches a Discord user mention like <@!12345> or <@12345>.
  * @see https://discordjs.guide/miscellaneous/parsing-mention-arguments.html#how-discord-mentions-work
  */
 export class DiscordUserMentionArg<T = string> extends RegexArg<T> {
-    constructor(name: string) {
+    constructor(...[name]: MentionArgs) {
         super(name, /^\<\@\!?(\d+)\>/, '<@12345>', { group: 1 })
     }
 
@@ -28,7 +31,7 @@ export class DiscordUserMentionArg<T = string> extends RegexArg<T> {
  * @see https://discordjs.guide/miscellaneous/parsing-mention-arguments.html#how-discord-mentions-work
  */
 export class DiscordRoleMentionArg<T = string> extends RegexArg<T> {
-    constructor(name: string) {
+    constructor(...[name]: MentionArgs) {
         super(name, /^\<\@\&(\d+)\>/, '<@&12345>', { group: 1 })
     }
 
@@ -50,7 +53,7 @@ export class DiscordRoleMentionArg<T = string> extends RegexArg<T> {
  * @see https://discordjs.guide/miscellaneous/parsing-mention-arguments.html#how-discord-mentions-work
  */
 export class DiscordChannelMentionArg<T = string> extends RegexArg<T> {
-    constructor(name: string) {
+    constructor(...[name]: MentionArgs) {
         super(name, /^\<\#(\d+)\>/, '<#12345>', { group: 1 })
     }
 
